5b23663ac0589ecc5f83543b5c42c78139faffac
sonjaehyuk <github@reception.sonjaehyuk.me>
2026-07-22 19:04:06 +0900

    개인정보처리방침 초안 작성

--- src/views/PrivacyView.vue
+++ src/views/PrivacyView.vue
@@ -3,5 +3,39 @@ import PageScaffold from '@/components/PageScaffold.vue'
 </script>
 
 <template>
-  <PageScaffold title="개인정보처리방침" />
+  <PageScaffold title="개인정보처리방침">
+  <div class="container">
+    <section class="mb-3">
+      <h2>개인정보의 처리 목적 </h2>
+      <div>
+        SAVER 서비스(이하 본 서비스)에서는 서비스의 원활한 이용과 남용 방지를 위해 주식회사 카카오(이하 카카오)로부터 사용자에 관한 닉네임과 프로필 사진(선택)을 제공받습니다.
+      </div>
+    </section>
+    <section class="mb-3">
+      <h2>개인정보의 처리 및 보유 기간</h2>
+      <div>
+        본 서비스에 저장되고 처리되는 개인정보는 사용자가 가입한 기간에 한정합니다.
+      </div>
+    </section>
+    <section class="mb-3">
+      <h2>처리하는 개인정보의 항목</h2>
+      <div>
+        <i>개인정보의 처리 목적</i> 항목에 나와있는 바와 동일합니다.
+      </div>
+    </section>
+    <section class="mb-3">
+      <h2>개인정보의 파기 절차 및 방법에 관한 사항</h2>
+      <div>
+        사용자는 언제든지 본 서비스에서 탈퇴할 수 있으며, <i>개인정보의 처리 및 보유 기간</i>에서 알린 바와 같이 탈퇴 즉시 개인정보는 파기됩니다.
+        이는 사용자의 개인정보가 저장되는 데이터베이스에서 즉시 삭제됨을 의미합니다. 파기 절차는 되돌릴 수 없으며, 다시 재가입할 때에도 사용자의 개인정보는 새롭게 처리됩니다.
+      </div>
+    </section>
+    <section class="mb-3">
+      <h2>개인정보의 열람청구를 접수·처리하는 부서</h2>
+      <div>
+        본 서비스는 손재혁이 단독으로 운영하고 관리하고 있으며, 개인정보의 열람청구를 관리 또한 마찬가지입니다.
+      </div>
+    </section>
+  </div>
+  </PageScaffold>
 </template>
